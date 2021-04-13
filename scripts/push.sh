f52a1a158658cc95388762479c2757303c04f2ad
Joseph Gilley <j@banditml.com>
2021-04-12 20:11:26 -0700

    safer ECR login

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 sha="$(git rev-parse --short HEAD)"
 echo "⏳ pushing $(tput bold)${sha}$(tput sgr0)..."
-docker push --all-tags 727419036083.dkr.ecr.us-east-1.amazonaws.com/postgres-dev
-echo "✅ done"
+if ! docker push --all-tags 727419036083.dkr.ecr.us-east-1.amazonaws.com/postgres-dev; then
+  echo "❌ try running \`./scripts/aws-docker-login.sh\` if its an AWS credential error"
+else
+  echo "✅ done"
+fi
