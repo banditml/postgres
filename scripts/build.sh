ec09248ab4914e178572fe106716eef39a258745
Joseph Gilley <j@banditml.com>
2021-04-14 14:59:35 -0700

    push tags with repo name

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 sha="$(git rev-parse --short HEAD)"
 echo "⏳ building $(tput bold)${sha}$(tput sgr0)..."
-docker build . -t postgres-dev:latest -t postgres-dev:"$sha"
+docker build . \
+  -t 727419036083.dkr.ecr.us-east-1.amazonaws.com/postgres-dev:latest \
+  -t 727419036083.dkr.ecr.us-east-1.amazonaws.com/postgres-dev:"$sha"
 echo "✅ done"
